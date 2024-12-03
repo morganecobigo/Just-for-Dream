93797a482c40d4d68444b9d664fb4343cdbf575e
cobigo <cobigomorgane@Mac.lan>
2024-12-03 16:12:10 +0100

    header

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./footer.css"
+
+function Footer() {
+  return (
+    <div className="footer">
+      <p className="copyright">© 2024 Morgane COBIGO tous droits réservés.</p>
+    </div>
+  )
+}
+
+export default Footer
