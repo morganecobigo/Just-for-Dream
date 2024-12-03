5a69f84cdf88d6187ee53d5e50bc6e7b025adb96
cobigo <cobigomorgane@Mac.lan>
2024-12-03 17:16:11 +0100

    modification home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,19 +1,19 @@
-import React from 'react' 
-import Hero from "../components/hero/Hero"
-import About from "../components/about/About"
-import Gallery from "../components/gallery/Gallery"
-import Services from "../components/services/Services"
-import Contact from "../components/contact/Contact"
+import React from "react"; 
+import Hero from "../../src/components/hero/Hero";
+import About from "../../src/components/about/About";
+import Gallery from "../../src/components/gallery/Gallery";
+import Services from "../../src/components/services/Services";
+import Contact from "../../src/components/contact/Contact";
 
-function Home() {
+const Home = () => {
   return (
     <div>
-    <Hero id="hero" />
-    <About id="about" />
-    <Gallery id="gallery" />
-    <Services id="services" />
-    <Contact id="contact" />
-  </div>
+      <Hero id="hero" />
+      <About id="about" />
+      <Gallery id="gallery" />
+      <Services id="services" />
+      <Contact id="contact" />
+    </div>
   );
 };
 
