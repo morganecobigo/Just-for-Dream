1ca58350b0f10aab0b5f4eb34792933d97330dc1
cobigo <cobigomorgane@Mac.lan>
2024-12-11 12:55:22 +0100

    partie formulaire de contact

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,9 +1,56 @@
-import React from 'react'
+import React from "react";
+import "./contact.css";
 
-function Contact() {
+const handleSubmit = (e) => {
+  e.preventDefault();
+  console.log("Formulaire soumis !");
+};
+
+const Contact = () => {
   return (
-    <div>Contact</div>
-  )
-}
+    <div className="contact">
+      <div className="form-image-wrapper">
+        <div className="form-container">
+          <h1>Contactez-moi</h1>
+          <form onSubmit={handleSubmit}>
+            <label htmlFor="nom">Nom</label>
+            <input type="text" id="nom" name="nom" placeholder="Votre nom" />
 
-export default Contact
+            <label htmlFor="prenom">Prénom</label>
+            <input
+              type="text"
+              id="prenom"
+              name="prenom"
+              placeholder="Votre prénom"
+            />
+
+            <label htmlFor="type">Vous êtes</label>
+            <select id="type" name="type">
+              <option value="particulier">Particulier</option>
+              <option value="professionnel">Professionnel</option>
+            </select>
+
+            <label htmlFor="email">E-mail</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="Votre e-mail"
+            />
+
+            <label htmlFor="message">Message</label>
+            <textarea
+              id="message"
+              name="message"
+              placeholder="Votre message"
+            ></textarea>
+
+            <button type="submit">Envoyer</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
